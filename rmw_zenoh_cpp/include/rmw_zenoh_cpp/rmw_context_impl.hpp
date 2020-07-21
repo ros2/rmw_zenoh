0757d7eee7895d323fe6862575754e9cf0cef25a
methylDragon <methylDragon@gmail.com>
2020-07-22 03:33:22 +0800

    Implement context struct

--- rmw_zenoh_cpp/include/rmw_zenoh_cpp/rmw_context_impl.hpp
+++ rmw_zenoh_cpp/include/rmw_zenoh_cpp/rmw_context_impl.hpp
@@ -1,10 +1,16 @@
 #ifndef RMW_ZENOH_CPP__RMW_CONTEXT_IMPL_HPP_
 #define RMW_ZENOH_CPP__RMW_CONTEXT_IMPL_HPP_
 
+extern "C"
+{
+  #include "zenoh/zenoh-ffi.h"
+}
+
 struct rmw_context_impl_t
 {
   /// Test data
   int context_data;
+  ZNSession * session;
 };
 
 #endif // RMW_ZENOH_CPP__RMW_CONTEXT_IMPL_HPP_
