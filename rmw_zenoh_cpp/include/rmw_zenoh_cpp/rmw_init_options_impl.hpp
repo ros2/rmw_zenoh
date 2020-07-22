05a300fbad23a7d65fad5b5576c9b89e59108c61
methylDragon <methylDragon@gmail.com>
2020-07-22 17:09:10 +0800

    Implement allocation for options

--- rmw_zenoh_cpp/include/rmw_zenoh_cpp/rmw_init_options_impl.hpp
+++ rmw_zenoh_cpp/include/rmw_zenoh_cpp/rmw_init_options_impl.hpp
@@ -3,8 +3,8 @@
 
 struct rmw_init_options_impl_t
 {
-  const char * session_locator;  // Zenoh session TCP locator
-  bool client_mode;  // Init Zenoh session in client mode
+  char * session_locator;  // Zenoh session TCP locator
+  char * mode;  // Zenoh session mode
 };
 
 #endif // RMW_ZENOH_CPP__RMW_INIT_OPTIONS_IMPL_HPP_
--- rmw_zenoh_cpp/src/rmw_init_options.cpp
+++ rmw_zenoh_cpp/src/rmw_init_options.cpp
@@ -7,12 +7,24 @@
 
 #include "rcutils/logging_macros.h"
 #include "rcutils/get_env.h"
+#include "rcutils/strdup.h"
 
 #include "rmw_zenoh_cpp/rmw_init_options_impl.hpp"
 #include "rmw_zenoh_cpp/identifier.hpp"
 
+// Helper case-insensitive string comparison
+int strcicmp(char const *a, char const *b)
+{
+    for (;; a++, b++) {
+        int d = tolower((unsigned char)*a) - tolower((unsigned char)*b);
+        if (d != 0 || !*a)
+            return d;
+    }
+}
+
 extern "C"
 {
+
 /// INIT OPTIONS ===============================================================
 // Initialize given init_options with the default values
 // and implementation specific values.
@@ -20,8 +32,7 @@ extern "C"
 // Note: You should call rmw_get_zero_initialized_init_options()
 // to get a zero initialized rmw_init_options_t struct first
 rmw_ret_t
-rmw_init_options_init(rmw_init_options_t * init_options,
-                      rcutils_allocator_t allocator)
+rmw_init_options_init(rmw_init_options_t * init_options, rcutils_allocator_t allocator)
 {
   RCUTILS_LOG_INFO_NAMED("rmw_zenoh_cpp", "rmw_init_options_init");
 
@@ -39,34 +50,64 @@ rmw_init_options_init(rmw_init_options_t * init_options,
   init_options->allocator = allocator;
   init_options->localhost_only = RMW_LOCALHOST_ONLY_DEFAULT;
 
-  // Populate Zenoh specific options
-  init_options->impl = new rmw_init_options_impl_t();
-
-  const char * session_env_value;
-  rcutils_get_env("RMW_ZENOH_SESSION_LOCATOR", &session_env_value);
-
-  if (strcmp(session_env_value, "") == 0) {  // if session_env_value == ""
-    init_options->impl->session_locator = NULL;
-  } else {
-    init_options->impl->session_locator = session_env_value;
-  }
-
-  const char * client_mode_env_value;
-  rcutils_get_env("RMW_ZENOH_USE_CLIENT_MODE", &client_mode_env_value);
-
-  if (strcmp(session_env_value, "") == 0
-      or strcmp(session_env_value, "0") == 0) {
-    init_options->impl->client_mode = false;
-  } else {
-    init_options->impl->client_mode = true;
-  }
-
   // NOTE(CH3): For DDS only
   // init_options->domain_id = RMW_DEFAULT_DOMAIN_ID;
 
   // NOTE(CH3): No security yet
-  // init_options->enclave = NULL;
+  // Include a "do-nothing" enclave for now
+  init_options->enclave = rcutils_strdup("/", allocator);
+
+  if (!init_options->enclave) {
+    RMW_SET_ERROR_MSG("failed to allocate options enclave");
+    return RMW_RET_BAD_ALLOC;
+  }
+
   // init_options->security_options = rmw_get_default_security_options();
+
+  // POPULATE ZENOH SPECIFIC OPTIONS ===========================================
+  // Allocate options implementation
+  init_options->impl = static_cast<rmw_init_options_impl_t *>(
+    allocator.allocate(sizeof(rmw_init_options_impl_t), allocator.state)
+  );
+
+  if (!init_options->impl) {
+    RMW_SET_ERROR_MSG("failed to allocate init_options impl");
+    init_options->impl = nullptr;  // To avoid free on uninitialized memory
+    return RMW_RET_BAD_ALLOC;
+  }
+
+  // Populate Zenoh session locator
+  const char * zenoh_session_env_value;
+  rcutils_get_env("RMW_ZENOH_SESSION_LOCATOR", &zenoh_session_env_value);
+
+  if (zenoh_session_env_value[0] == '\0') {
+    init_options->impl->session_locator = nullptr;
+  } else {
+    init_options->impl->session_locator = rcutils_strdup(zenoh_session_env_value, allocator);
+
+    if (!init_options->impl->session_locator) {
+      RMW_SET_ERROR_MSG("failed to allocate RMW_ZENOH_SESSION_LOCATOR");
+      return RMW_RET_BAD_ALLOC;
+    }
+  }
+
+  // Populate Zenoh session mode
+  const char * zenoh_mode_env_value;
+  rcutils_get_env("RMW_ZENOH_MODE", &zenoh_mode_env_value);
+
+  // Case insensitive comparisons
+  if (strcicmp(zenoh_mode_env_value, "CLIENT") == 0) {
+    init_options->impl->mode = rcutils_strdup("CLIENT", allocator);
+  } else if (strcicmp(zenoh_mode_env_value, "ROUTER") == 0) {
+    init_options->impl->mode = rcutils_strdup("ROUTER", allocator);
+  } else {
+    init_options->impl->mode = rcutils_strdup("PEER", allocator);
+  }
+
+  if (!init_options->impl->mode) {
+    RMW_SET_ERROR_MSG("failed to allocate RMW_ZENOH_MODE");
+    return RMW_RET_BAD_ALLOC;
+  }
 
   return RMW_RET_OK;
 }
@@ -90,17 +131,45 @@ rmw_init_options_copy(const rmw_init_options_t * src, rmw_init_options_t * dst)
     return RMW_RET_INVALID_ARGUMENT;
   }
 
-  *dst = *src;
+  const rcutils_allocator_t * allocator = &src->allocator;
+  RCUTILS_CHECK_ALLOCATOR(allocator, return RMW_RET_INVALID_ARGUMENT);
+
+  // Copy dynamically allocated strings
+  rmw_init_options_t tmp = *src;
+
+  tmp.enclave = rcutils_strdup(tmp.enclave, *allocator);
+  if (nullptr != src->enclave && nullptr == tmp.enclave) {
+    RMW_SET_ERROR_MSG("failed to allocate options enclave");
+    return RMW_RET_BAD_ALLOC;
+  }
+
+  tmp.impl->session_locator = rcutils_strdup(tmp.impl->session_locator, *allocator);
+  if (nullptr != src->impl->session_locator && nullptr == tmp.impl->session_locator) {
+    RMW_SET_ERROR_MSG("failed to allocate RMW_ZENOH_SESSION_LOCATOR");
+    return RMW_RET_BAD_ALLOC;
+  }
+
+  tmp.impl->mode = rcutils_strdup(tmp.impl->mode, *allocator);
+  if (nullptr != src->impl->mode && nullptr == tmp.impl->mode) {
+    RMW_SET_ERROR_MSG("failed to allocate RMW_ZENOH_MODE");
+    return RMW_RET_BAD_ALLOC;
+  }
+
+  // NOTE(CH3): No security yet
+  // tmp.security_options = rmw_get_zero_initialized_security_options();
+  // rmw_ret_t ret =
+  //   rmw_security_options_copy(&src->security_options, allocator, &tmp.security_options);
+  // if (RMW_RET_OK != ret) {
+  //   allocator->deallocate(tmp.enclave, allocator->state);
+  //   return ret;
+  // }
+
+  *dst = tmp;
   return RMW_RET_OK;
 }
 
 /// FINALIZE OPTIONS ===========================================================
-// Finalize the given init_options.
-//
-// NOTE(CH3): It looks like the standard use of this function is for checking
-// and then resetting init options to zero?? The docs say it is for finalizing.
-//
-// I don't understand why, so I just commented out the zero init here..
+// Finalize the given init_options. (Cleanup and deallocation.)
 rmw_ret_t
 rmw_init_options_fini(rmw_init_options_t * init_options)
 {
@@ -120,14 +189,11 @@ rmw_init_options_fini(rmw_init_options_t * init_options)
   rcutils_allocator_t * allocator = &init_options->allocator;
   RCUTILS_CHECK_ALLOCATOR(allocator, return RMW_RET_INVALID_ARGUMENT);
 
-  // NOTE(CH3): No security yet
-  // allocator->deallocate(init_options->enclave, allocator->state);
-  // rmw_ret_t ret = rmw_security_options_fini(&init_options->security_options,
-  //                                           allocator);
+  allocator->deallocate(init_options->enclave, allocator->state);
+  allocator->deallocate(init_options->impl->session_locator, allocator->state);
+  allocator->deallocate(init_options->impl->mode, allocator->state);
 
-  // NOTE(CH3): This line seems to be in a lot of existing RMW implementations
-  // but it looks like it just resets the init options?? What's up with that?
-  // *init_options = rmw_get_zero_initialized_init_options();
+  *init_options = rmw_get_zero_initialized_init_options();
 
   // return ret;
   return RMW_RET_OK;
