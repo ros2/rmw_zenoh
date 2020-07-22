efe35e59620f946e6fa31b8c41dbc4571d7b40f6
methylDragon <methylDragon@gmail.com>
2020-07-23 04:15:33 +0800

    Add node implementation

---
+++ rmw_zenoh_cpp/include/rmw_zenoh_cpp/rmw_node_impl.hpp
@@ -0,0 +1,14 @@
+#ifndef RMW_ZENOH_CPP__RMW_NODE_IMPL_HPP_
+#define RMW_ZENOH_CPP__RMW_NODE_IMPL_HPP_
+
+extern "C"
+{
+  #include "zenoh/zenoh-ffi.h"
+}
+
+struct rmw_node_impl_t
+{
+  
+};
+
+#endif // RMW_ZENOH_CPP__RMW_NODE_IMPL_HPP_
