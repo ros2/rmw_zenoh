86ee47d48bad1166010683e6b2d7d579afde0704
methylDragon <methylDragon@gmail.com>
2020-07-28 17:09:01 +0800

    Add publisher event stub

--- rmw_zenoh_cpp/src/rmw_event.cpp
+++ rmw_zenoh_cpp/src/rmw_event.cpp
@@ -1,11 +1,16 @@
+// Docs: http://docs.ros2.org/latest/api/rmw/event_8h.html
+
 #include "rcutils/logging_macros.h"
+
+#include "rmw/impl/cpp/macros.hpp"
 #include "rmw/error_handling.h"
 #include "rmw/rmw.h"
 #include "rmw/event.h"
 
+#include "rmw_zenoh_cpp/identifier.hpp"
+
 extern "C"
 {
-
 rmw_ret_t
 rmw_take_event(const rmw_event_t * event_handle, void * event_info, bool * taken)
 {
@@ -22,12 +27,30 @@ rmw_publisher_event_init(
   const rmw_publisher_t * publisher,
   rmw_event_type_t event_type)
 {
-  (void)event;
-  (void)publisher;
-  (void)event_type;
-  RCUTILS_LOG_INFO_NAMED("rmw_zenoh_cpp", "rmw_publisher_event_init");
-  return RMW_RET_ERROR;
+  RMW_CHECK_ARGUMENT_FOR_NULL(publisher, RMW_RET_INVALID_ARGUMENT);
+  RMW_CHECK_ARGUMENT_FOR_NULL(event, RMW_RET_INVALID_ARGUMENT);
+  RMW_CHECK_ARGUMENT_FOR_NULL(event_type, RMW_RET_INVALID_ARGUMENT);
+  RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
+    publisher,
+    publisher->implementation_identifier,
+    eclipse_zenoh_identifier,
+    return RMW_RET_INCORRECT_RMW_IMPLEMENTATION
+  );
+
+  // TODO(CH3) NOTE(CH3): Check if event type is supported
+  // Most likely no. It seems to be a DDS QoS specific thing
+  // if (!internal::is_event_supported(event_type)) {
+  //   RMW_SET_ERROR_MSG_WITH_FORMAT_STRING("provided event_type is not supported by %s", identifier);
+  //   return RMW_RET_UNSUPPORTED;
+  // }
+
+  event->implementation_identifier = publisher->implementation_identifier;
+  event->data = publisher->data;
+  event->event_type = event_type;
+
+  return RMW_RET_OK;
 }
+
 
 rmw_ret_t
 rmw_subscription_event_init(
