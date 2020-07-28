62bed242fe79b49527cd9e2ee5e8967c0c186d35
methylDragon <methylDragon@gmail.com>
2020-07-28 17:09:11 +0800

    Implement publisher GID getter

--- rmw_zenoh_cpp/src/rmw_gids.cpp
+++ rmw_zenoh_cpp/src/rmw_gids.cpp
@@ -1,19 +1,46 @@
 #include "rcutils/logging_macros.h"
-#include "rmw/error_handling.h"
-#include "rmw/rmw.h"
-#include "rmw/names_and_types.h"
+
 #include "rmw/topic_endpoint_info_array.h"
+#include "rmw/impl/cpp/macros.hpp"
+#include "rmw/error_handling.h"
+#include "rmw/names_and_types.h"
+#include "rmw/rmw.h"
+
+#include "rmw_zenoh_cpp/zenoh_pubsub.hpp"
+#include "rmw_zenoh_cpp/identifier.hpp"
 
 extern "C"
 {
-
+/// GET PUBLISHER GID ==========================================================
+// rmw_gid_t Doc: http://docs.ros2.org/latest/api/rmw/structrmw__gid__t.html
 rmw_ret_t
 rmw_get_gid_for_publisher(const rmw_publisher_t * publisher, rmw_gid_t * gid)
 {
-  (void)publisher;
-  (void)gid;
-  RCUTILS_LOG_INFO_NAMED("rmw_zenoh_cpp", "rmw_get_gid_for_publisher");
-  return RMW_RET_ERROR;
+  if (!publisher) {
+    RMW_SET_ERROR_MSG("publisher is null");
+    return RMW_RET_ERROR;
+  }
+
+  if (publisher->implementation_identifier != eclipse_zenoh_identifier) {
+    RMW_SET_ERROR_MSG("publisher handle not from this implementation");
+    return RMW_RET_ERROR;
+  }
+
+  if (!gid) {
+    RMW_SET_ERROR_MSG("gid is null");
+    return RMW_RET_ERROR;
+  }
+
+  // Copy size_t to gid member
+  // NOTE(CH3): I am unsure if this is copying properly!!
+  memset(gid->data, 0, sizeof(gid->data));
+  memcpy(
+    gid->data,
+    &static_cast<rmw_publisher_data_t *>(publisher->data)->zn_topic_id_,
+    sizeof(static_cast<rmw_publisher_data_t *>(publisher->data)->zn_topic_id_)
+  );
+
+  return RMW_RET_OK;
 }
 
 rmw_ret_t
