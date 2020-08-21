8bc43f0a5394f7f7c927d1d6325e917e24430480
methylDragon <methylDragon@gmail.com>
2020-08-21 19:19:36 +0800

    Neaten service impl header

--- rmw_zenoh_cpp/src/impl/service_impl.hpp
+++ rmw_zenoh_cpp/src/impl/service_impl.hpp
@@ -16,56 +16,24 @@ extern "C"
 
 struct rmw_service_data_t
 {
-    static void zn_request_sub_callback(const zn_sample * sample);
+  static void zn_request_sub_callback(const zn_sample * sample);
 
-    // Serialized ROS request messages
-    static std::unordered_map<std::string, std::vector<unsigned char> > zn_request_messages_;
+  // Serialized ROS request messages
+  static std::unordered_map<std::string, std::vector<unsigned char> > zn_request_messages_;
 
-    const void * request_type_support_impl_;
-    const void * response_type_support_impl_;
-    const char * typesupport_identifier_;
+  const void * request_type_support_impl_;
+  const void * response_type_support_impl_;
+  const char * typesupport_identifier_;
 
-    rmw_zenoh_cpp::TypeSupport * request_type_support_;
-    rmw_zenoh_cpp::TypeSupport * response_type_support_;
+  rmw_zenoh_cpp::TypeSupport * request_type_support_;
+  rmw_zenoh_cpp::TypeSupport * response_type_support_;
 
-    ZNSession * zn_session_;
-    ZNQueryable * zn_queryable_;
-    ZNSubscriber * zn_request_subscriber_;
-    size_t zn_response_topic_id_;
+  ZNSession * zn_session_;
+  ZNQueryable * zn_queryable_;
+  ZNSubscriber * zn_request_subscriber_;
+  size_t zn_response_topic_id_;
 
-    const rmw_node_t * node_;
+  const rmw_node_t * node_;
 };
-
-// struct rmw_publisher_data_t
-// {
-//   const void * type_support_impl_;
-//   const char * typesupport_identifier_;
-//
-//   rmw_zenoh_cpp::TypeSupport * type_support_;
-//
-//   size_t zn_topic_id_;
-//   ZNSession * zn_session_;
-//
-//   const rmw_node_t * node_;
-// };
-//
-// // Functionally a struct. But with a method for handling incoming Zenoh messages
-// struct rmw_subscription_data_t
-// {
-//   // TODO(CH3): If needed, implement lock guards/mutexes to prevent race conditions
-//   static void zn_sub_callback(const zn_sample * sample);
-//
-//   // Serialized ROS message!
-//   static std::unordered_map<std::string, std::pair<std::vector<unsigned char>, size_t> > zn_messages_;
-//
-//   const void * type_support_impl_;
-//   const char * typesupport_identifier_;
-//
-//   rmw_zenoh_cpp::TypeSupport * type_support_;
-//   const rmw_node_t * node_;
-//
-//   ZNSession * zn_session_;
-//   ZNSubscriber * zn_subscriber_;
-// };
 
 #endif // RMW_ZENOH_CPP__ZENOH_SERVICE_HPP_
