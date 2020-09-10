810d89ac1e5cf6e8b01be9a7dbdd9694f5295392
Morgan Quigley <morgan@osrfoundation.org>
2020-09-10 17:20:43 +0800

    Update rmw_zenoh_cpp/src/impl/debug_helpers.cpp
    Co-authored-by: Geoffrey Biggs <gbiggs@killbots.net>

--- rmw_zenoh_cpp/src/impl/debug_helpers.cpp
+++ rmw_zenoh_cpp/src/impl/debug_helpers.cpp
@@ -18,8 +18,7 @@
 void rmw_zenoh_cpp::log_debug_qos_profile(const rmw_qos_profile_t * qos_profile)
 {
   const char * reliability_string = NULL;
-  switch (qos_profile->reliability)
-  {
+  switch (qos_profile->reliability) {
     case RMW_QOS_POLICY_RELIABILITY_SYSTEM_DEFAULT:
       reliability_string = "system default";
       break;
