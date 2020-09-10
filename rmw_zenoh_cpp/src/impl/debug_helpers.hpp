cdc9b4ea0bcabd5ec67aa9f4bf29776d30d4bfb0
Morgan Quigley <morgan@osrfoundation.org>
2020-09-10 09:40:05 +0800

    add helper files
    Signed-off-by: Morgan Quigley <morgan@osrfoundation.org>

---
+++ rmw_zenoh_cpp/src/impl/debug_helpers.hpp
@@ -0,0 +1,13 @@
+#ifndef RMW_ZENOH_CPP__DEBUG_HELPERS_HPP_
+#define RMW_ZENOH_CPP__DEBUG_HELPERS_HPP_
+
+#include "rmw/types.h"
+
+namespace rmw_zenoh_cpp
+{
+
+void log_debug_qos_profile(const rmw_qos_profile_t * qos_profile);
+
+}
+
+#endif
