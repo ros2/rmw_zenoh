cb5003b086ddc9940608896f5d737b7f2f625995
Geoffrey Biggs <gbiggs@killbots.net>
2020-09-10 14:47:46 +0900

    Add OSRF as copyright

--- rmw_zenoh_cpp/include/rmw_zenoh_cpp/TypeSupport.hpp
+++ rmw_zenoh_cpp/include/rmw_zenoh_cpp/TypeSupport.hpp
@@ -1,4 +1,5 @@
 // Copyright 2016-2018 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+// Copyright 2020 Open Source Robotics Foundation, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
