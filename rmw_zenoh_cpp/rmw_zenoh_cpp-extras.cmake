89dc940202ac9f3fff9438ed1e4f7f87281ad7be
Esteve Fernandez <esteve@apache.org>
2020-11-20 15:26:59 +0100

    Export dependencies only once

--- rmw_zenoh_cpp/rmw_zenoh_cpp-extras.cmake
+++ rmw_zenoh_cpp/rmw_zenoh_cpp-extras.cmake
@@ -14,9 +14,26 @@
 
 # copied from rmw_fastrtps_cpp/rmw_fastrtps_cpp-extras.cmake
 
-find_package(zenoh_vendor REQUIRED)
-find_package(zenoh REQUIRED)
+find_package(zenoh_vendor QUIET)
+if(zenoh_vendor_FOUND)
+  find_package(zenoh QUIET)
+  if(zenoh_FOUND)
+    list(APPEND rmw_zenoh_cpp_INCLUDE_DIRS ${zenoh_INCLUDE_DIRS})
+    # specific order: dependents before dependencies
+    list(APPEND rmw_zenoh_cpp_LIBRARIES ${zenoh_LIBRARIES})
+  endif()
+endif()
 
-list(APPEND rmw_zenoh_cpp_INCLUDE_DIRS ${zenoh_INCLUDE_DIRS})
-# specific order: dependents before dependencies
-list(APPEND rmw_zenoh_cpp_LIBRARIES ${zenoh_LIBRARIES})
+find_package(zenoh_pico_vendor QUIET)
+if(zenoh_pico_vendor_FOUND)
+  find_package(zenoh_pico QUIET)
+  if(zenoh_pico_FOUND)
+    list(APPEND rmw_zenoh_cpp_INCLUDE_DIRS ${zenoh_pico_INCLUDE_DIRS})
+    # specific order: dependents before dependencies
+    list(APPEND rmw_zenoh_cpp_LIBRARIES ${zenoh_pico_LIBRARIES})
+  endif()
+endif()
+
+if(NOT zenoh_FOUND AND NOT zenoh_pico_FOUND)
+  message(FATAL_ERROR "rmw_zenoh_cpp needs at least zenoh-c or zenoh-pico, could not find either")
+endif()
