210aec32b59a9965dd163dc3d51836a2d3dec462
Yadunund <yadunund@gmail.com>
2024-02-27 23:09:27 +0800

    Make zenoh router configurable using envar
    Signed-off-by: Yadunund <yadunund@gmail.com>

--- rmw_zenoh_cpp/src/detail/zenoh_config.cpp
+++ rmw_zenoh_cpp/src/detail/zenoh_config.cpp
@@ -22,48 +22,69 @@
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <rmw/impl/cpp/macros.hpp>
 
-#include "identifier.hpp"
-
+///==============================================================================
 namespace
 {
-
-/// Env var to set the path to the zenoh configuration file.
-static constexpr const char * kZenohConfigFileEnvVar = "RMW_ZENOH_CONFIG_FILE";
 /// The name of the default configuration file for the default zenoh session configuration.
-static constexpr const char * const kDefaultZenohConfigFileName =
-  "DEFAULT_RMW_ZENOH_SESSION_CONFIG.json5";
+static const std::unordered_map<ConfigurableEntity, const char *> default_config_filenames = {
+  {ConfigurableEntity::Session, "DEFAULT_RMW_ZENOH_SESSION_CONFIG.json5"},
+  {ConfigurableEntity::Router, "DEFAULT_RMW_ZENOH_ROUTER_CONFIG.json5"}
+};
 
-}  // namespace
-
-rmw_ret_t get_z_config(z_owned_config_t * config)
+rmw_ret_t _get_z_config(
+  const char * envar_name,
+  const char * default_uri,
+  z_owned_config_t * config)
 {
-  const char * zenoh_config_path;
+  const char * configured_uri;
+  const char * envar_uri;
   // Get the path to the zenoh configuration file from the environment variable.
-  if (NULL != rcutils_get_env(kZenohConfigFileEnvVar, &zenoh_config_path)) {
+  if (NULL != rcutils_get_env(envar_name, &envar_uri)) {
     // NULL is returned if everything is ok.
     RCUTILS_LOG_ERROR_NAMED(
-      "ZenohConfiguration", "Env Var '%s' can't be read.", kZenohConfigFileEnvVar);
+      "rmw_zenoh_cpp", "Envar %s cannot be read.", envar_name);
     return RMW_RET_ERROR;
   }
-
   // If the environment variable contains a path to a file, try to read the configuration from it.
-  if (zenoh_config_path[0] != '\0') {
+  if (envar_uri[0] != '\0') {
     // If the environment variable is set, try to read the configuration from the file.
-    *config = zc_config_from_file(zenoh_config_path);
+    *config = zc_config_from_file(envar_uri);
+    configured_uri = envar_uri;
   } else {
     // If the environment variable is not set use internal configuration
-    static const std::string path_to_config_folder =
-      ament_index_cpp::get_package_share_directory(rmw_zenoh_identifier) + "/config/";
-    const std::string default_zconfig_path = path_to_config_folder + kDefaultZenohConfigFileName;
-    *config = zc_config_from_file(default_zconfig_path.c_str());
+    *config = zc_config_from_file(default_uri);
+    configured_uri = default_uri;
   }
-
   // Verify that the configuration is valid.
   if (!z_config_check(config)) {
     RCUTILS_LOG_ERROR_NAMED(
-      "ZenohConfiguration",
-      "Configuration is not valid. Please check the zenoh configuration file.");
+      "rmw_zenoh_cpp",
+      "Invalid configuration file %s", configured_uri);
     return RMW_RET_ERROR;
   }
+  RCUTILS_LOG_DEBUG_NAMED(
+    "rmw_zenoh_cpp",
+    "configured using configuration file %s", configured_uri);
   return RMW_RET_OK;
+}
+}  // namespace
+
+///==============================================================================
+rmw_ret_t get_z_config(const ConfigurableEntity & entity, z_owned_config_t * config)
+{
+  auto entity_envar_it = envar_map.find(entity);
+  auto default_filename_it = default_config_filenames.find(entity);
+  if (entity_envar_it == envar_map.end() ||
+    default_filename_it == default_config_filenames.end() )
+  {
+    RCUTILS_LOG_ERROR_NAMED(
+      "rmw_zenoh_cpp", "get_z_config called with invalid ConfigurableEntity.");
+    return RMW_RET_ERROR;
+  }
+  // Get the absolute path to the default configuration file.
+  static const std::string path_to_config_folder =
+    ament_index_cpp::get_package_share_directory("rmw_zenoh_cpp") + "/config/";
+  const std::string default_config_path = path_to_config_folder + default_filename_it->second;
+
+  return _get_z_config(entity_envar_it->second, default_config_path.c_str(), config);
 }
--- rmw_zenoh_cpp/src/detail/zenoh_config.hpp
+++ rmw_zenoh_cpp/src/detail/zenoh_config.hpp
@@ -15,20 +15,38 @@
 #ifndef DETAIL__ZENOH_CONFIG_HPP_
 #define DETAIL__ZENOH_CONFIG_HPP_
 
+#include <unordered_map>
+
 #include <zenoh.h>
 
 #include "rmw/ret_types.h"
 
-/// Get the zenoh configuration for a session.
+///==============================================================================
+enum class ConfigurableEntity : uint8_t
+{
+  Invalid = 0,
+  Session,
+  Router
+};
+
+///==============================================================================
+/// Environment variable name that stores the absolute path to the Zenoh config
+/// for respective entities.
+static const std::unordered_map<ConfigurableEntity, const char *> envar_map = {
+  {ConfigurableEntity::Session, "ZENOH_SESSION_CONFIG_URI"},
+  {ConfigurableEntity::Router, "ZENOH_ROUTER_CONFIG_URI"}
+};
+
+/// Get the zenoh configuration for a configurable entity.
 /// @details The behavior is as follows:
-///   - If the environment variable `RMW_ZENOH_CONFIG_FILE` is set, it will
-///     be used as the path to the zenoh configuration file.
-///     - In case there is an error reading the file, the internal configuration will be used.
-///   - If the environment variable `RMW_ZENOH_CONFIG_FILE` is not set, the
-///     configuration will be read from the internal configuration.
-///   - If internal configuration is not available, a zenoh default configuration is used.
+///   - If the environment variable for the entity is set, the z_owned_config_t
+///     is configured as per the configuration file retrieved.
+///   - If the environment variable is not set, the z_owned_config_t
+///     is configured using the rmw_zenoh default configuration file.
+/// @param entity The zenoh entity to be configured.
 /// @param config The zenoh configuration to be filled.
 /// @returns `RMW_RET_OK` if the configuration was successfully loaded.
-rmw_ret_t get_z_config(z_owned_config_t * config);
+[[nodiscard]]
+rmw_ret_t get_z_config(const ConfigurableEntity & entity, z_owned_config_t * config);
 
 #endif  // DETAIL__ZENOH_CONFIG_HPP_
