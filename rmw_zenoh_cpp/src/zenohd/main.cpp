b6a881c694163e7084521e3ebbfb1fbd72ba5a43
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2024-06-01 15:29:25 -0400

    print router id on startup

--- rmw_zenoh_cpp/src/zenohd/main.cpp
+++ rmw_zenoh_cpp/src/zenohd/main.cpp
@@ -32,6 +32,7 @@
 #include <ament_index_cpp/get_package_share_directory.hpp>
 
 #include "../detail/zenoh_config.hpp"
+#include "../detail/liveliness_utils.hpp"
 
 #include "rmw/error_handling.h"
 
@@ -188,7 +189,9 @@ int main(int argc, char ** argv)
     printf("Unable to open router session!\n");
     return 1;
   }
-
+  printf(
+    "router id: %s\n",
+    rmw_zenoh_cpp::liveliness::zid_to_str(z_info_zid(z_session_loan(&s))).c_str());
 #ifdef _WIN32
   SetConsoleCtrlHandler(quit, TRUE);
 #else
