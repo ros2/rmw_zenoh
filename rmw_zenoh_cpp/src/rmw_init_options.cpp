adfdf9557725027af173487aad0411fc9fd20ff7
CihatAltiparmak <cihataltiparmak1@gmail.com>
2024-08-03 01:38:34 +0300

    remove rmw_localhost_only_t.

--- rmw_zenoh_cpp/src/rmw_init_options.cpp
+++ rmw_zenoh_cpp/src/rmw_init_options.cpp
@@ -47,7 +47,6 @@ rmw_init_options_init(rmw_init_options_t * init_options, rcutils_allocator_t all
   init_options->enclave = nullptr;
   init_options->domain_id = RMW_DEFAULT_DOMAIN_ID;
   init_options->security_options = rmw_get_default_security_options();
-  init_options->localhost_only = RMW_LOCALHOST_ONLY_DEFAULT;
   init_options->discovery_options = rmw_get_zero_initialized_discovery_options();
   return rmw_discovery_options_init(&(init_options->discovery_options), 0, &allocator);
 }
