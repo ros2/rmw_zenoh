bfc065f8c80cc3bc4d1963dc44ac5a472460903a
Chris Lalancette <clalancette@gmail.com>
2024-10-16 16:00:46 -0400

    Cleanup SubscriptionData creation and destruction. (#299)
    * Cleanup SubscriptionData creation and destruction.
    
    In particular, make SubscriptionData::init/shutdown
    mirror images, in that they handle all of their internal
    data.  That means that rmw_destroy_subscription() really
    only calls delete_sub_data().
    
    To achieve this, we had to split out SubscriptionData::init
    from construction, since we rely on shared pointers, but
    you can't use shared_from_this() in the constructor.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>
    
    * Feedback from review.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>
    
    ---------
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- rmw_zenoh_cpp/src/detail/rmw_subscription_data.hpp
+++ rmw_zenoh_cpp/src/detail/rmw_subscription_data.hpp
@@ -42,7 +42,7 @@
 namespace rmw_zenoh_cpp
 {
 ///=============================================================================
-class SubscriptionData final
+class SubscriptionData final : public std::enable_shared_from_this<SubscriptionData>
 {
 public:
   struct Message
@@ -132,6 +132,8 @@ private:
     const void * type_support_impl,
     std::unique_ptr<MessageTypeSupport> type_support);
 
+  bool init();
+
   // Internal mutex.
   mutable std::mutex mutex_;
   // The parent node.
@@ -158,6 +160,8 @@ private:
   std::shared_ptr<EventsManager> events_mgr_;
   // Shutdown flag.
   bool is_shutdown_;
+  // Whether the object has ever successfully been initialized.
+  bool initialized_;
 };
 using SubscriptionDataPtr = std::shared_ptr<SubscriptionData>;
 using SubscriptionDataConstPtr = std::shared_ptr<const SubscriptionData>;
