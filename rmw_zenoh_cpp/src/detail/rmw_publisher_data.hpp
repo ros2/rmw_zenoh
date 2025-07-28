5f4ab0ee907ab4119e44050817d741562d5d9582
yellowhatter <bannov.dy@gmail.com>
2025-07-28 13:42:07 +0300

    Fix compilation error

--- rmw_zenoh_cpp/src/detail/rmw_publisher_data.hpp
+++ rmw_zenoh_cpp/src/detail/rmw_publisher_data.hpp
@@ -57,13 +57,13 @@ public:
   // Publish a ROS message.
   rmw_ret_t publish(
     const void * ros_message,
-    const std::optional<ShmContext> & shm
+    const std::shared_ptr<ShmContext> shm
   );
 
   // Publish a serialized ROS message.
   rmw_ret_t publish_serialized_message(
     const rmw_serialized_message_t * serialized_message,
-    const std::optional<ShmContext> & shm
+    const std::shared_ptr<ShmContext> shm
   );
 
   // Get a copy of the gid_hash of this PublisherData's liveliness::Entity.
