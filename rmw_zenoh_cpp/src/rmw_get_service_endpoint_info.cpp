94df10f1c00321ded81f194c614070a9aef8f9bb
Minju, Lee <dlalswn531@naver.com>
2025-11-10 10:31:53 +0900

    Delete restricting right comment
    Signed-off-by: Minju, Lee <dlalswn531@naver.com>

--- rmw_zenoh_cpp/src/rmw_get_service_endpoint_info.cpp
+++ rmw_zenoh_cpp/src/rmw_get_service_endpoint_info.cpp
@@ -1,4 +1,4 @@
-// Copyright 2025 Minju Lee (이민주). All rights reserved.
+// Copyright 2025 Minju Lee (이민주).
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
