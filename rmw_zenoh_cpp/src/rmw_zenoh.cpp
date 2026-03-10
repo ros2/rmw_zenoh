483fe547114610736fdbc38df3ebd903115740f1
Yuyuan Yuan <az6980522@gmail.com>
2026-03-10 16:20:00 +0800

    fix: populate reception_sequence_number and advertise sequence number features (#920)
    (cherry picked from commit 3cbcffb735d5c2361c13dd6babec9fbb40faf3f4)

--- rmw_zenoh_cpp/src/rmw_zenoh.cpp
+++ rmw_zenoh_cpp/src/rmw_zenoh.cpp
@@ -140,9 +140,9 @@ bool rmw_feature_supported(rmw_feature_t feature)
 {
   switch (feature) {
     case RMW_FEATURE_MESSAGE_INFO_PUBLICATION_SEQUENCE_NUMBER:
-      return false;
+      return true;
     case RMW_FEATURE_MESSAGE_INFO_RECEPTION_SEQUENCE_NUMBER:
-      return false;
+      return true;
     case RMW_MIDDLEWARE_SUPPORTS_TYPE_DISCOVERY:
       return true;
     case RMW_MIDDLEWARE_CAN_TAKE_DYNAMIC_MESSAGE:
